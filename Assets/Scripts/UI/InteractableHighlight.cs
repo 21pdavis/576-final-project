9eb70dab651abf488467b37ef491885980d9c65d
Paul Davis <paulcdavis135@gmail.com>
2023-12-15 04:53:37 -0500

    removing some dead commented code

--- Assets/Scripts/UI/InteractableHighlight.cs
+++ Assets/Scripts/UI/InteractableHighlight.cs
@@ -18,15 +18,12 @@ public class InteractableHighlight : MonoBehaviour
     private GameObject liveIndicator;
     private float bounceRadians;
     private float startY;
-    //private float endY;
 
     // Start is called before the first frame update
     void Start()
     {
-        //line = GetComponent<LineRenderer>();
         bounceRadians = 0f;
         startY = transform.position.y + indicatorYOffset;
-        //endY = startY + indicatorLength;
     }
 
     private void Update()
@@ -40,23 +37,6 @@ public class InteractableHighlight : MonoBehaviour
                 liveIndicator.transform.position.z
             );
         }
-
-        //if (line.positionCount > 0)
-        //{
-        //    Vector3 pos0 = line.GetPosition(0);
-        //    Vector3 pos1 = line.GetPosition(1);
-
-        //    bounceRadians = (bounceRadians + Time.deltaTime * bounceSpeed * Mathf.PI / 180) % (2 * Mathf.PI);
-
-        //    pos0.y += Mathf.Sin(bounceRadians);
-        //    pos1.y += Mathf.Sin(bounceRadians);
-
-        //    line.SetPositions(new Vector3[] 
-        //    {
-        //        pos0,
-        //        pos1
-        //    });
-        //}
     }
 
     private void OnMouseEnter()
@@ -68,21 +48,11 @@ public class InteractableHighlight : MonoBehaviour
             Quaternion.identity,
             transform.parent
         );
-
-        //line.positionCount = 2;
-        //line.SetPositions(new Vector3[]
-        //{
-        //    new Vector3(transform.position.x, startY, transform.position.z),
-        //    new Vector3(transform.position.x, endY, transform.position.z)
-        //});
     }
 
     private void OnMouseExit()
     {
         Debug.Log("Destroying");
         Destroy(liveIndicator);
-
-        //line.positionCount = 0;
-        //bounceRadians = 0f;
     }
 }
